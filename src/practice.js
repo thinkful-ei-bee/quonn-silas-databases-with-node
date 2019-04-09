fa7f37ae23e63fbb5715dd14248f07293e6f34ff
Silas <silasishallahan@gmail.com>
2019-04-09 08:37:05 -0700

    init

---
+++ src/practice.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-console */
+'use strict';
+
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+});
+
+
+function searchByProduceName(searchTerm) {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function paginateProducts(page) {
+  const productsPerPage = 10;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      knexInstance.raw('now() - \'?? days\'::INTERVAL', days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC' },
+    ])
+    .then(result => {
+      console.log(result);
+    });
+}
+
+paginateProducts(2);
+
+console.log('connection successful');
